ce5d762a1c012aed5d05f6cfc0ab0130d39e5884
Taj <tajulislamj200@gmail.com>
2025-06-26 17:33:52 +0530

    v1.2.43

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -5,17 +5,17 @@ describe('Ambient Music Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
   it('Sample test: Extension should be present', async () => {
-    const extension = vscode.extensions.getExtension('taj154dev.ambient-music-extension');
+    const extension = vscode.extensions.getExtension('taj154dev.ambient-music-vs-extension');
     assert.ok(extension);
   });
 
   it('Sample test: Extension activates without error', async () => {
-    const extension = vscode.extensions.getExtension('taj154dev.ambient-music-extension');
+    const extension = vscode.extensions.getExtension('taj154dev.ambient-music-vs-extension');
     await extension?.activate();
     assert.strictEqual(extension?.isActive, true);
   });
   it('Ambient Music server should start (based on port message)', async () => {
-    const ext = vscode.extensions.getExtension('taj154dev.ambient-music-extension');
+    const ext = vscode.extensions.getExtension('taj154dev.ambient-music-vs-extension');
     await ext?.activate();
     assert.ok(ext?.isActive, 'Extension active but server status unknown');
   });
