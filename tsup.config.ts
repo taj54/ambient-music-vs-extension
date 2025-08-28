56558a444ce675eeed1df2206fc8b3d3101f9a5b
Taj <tajulislamj200@gmail.com>
2025-08-28 18:23:48 +0530

    build: update build system to use tsup
    - Replaced tsc with tsup for building
    - Updated package.json scripts
    - Added tsup.config.ts
    - Added author and license to package.json
    - Improved package description

---
+++ tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsup';
+import pkg from './package.json';
+
+export default defineConfig({
+  entry: ['src/extension.ts', 'test/runTest.ts', 'test/suite/index.ts', 'test/extension.test.ts'],
+  outDir: 'dist',
+  target: 'es2020',
+  format: ['cjs'],
+  sourcemap: true,
+  clean: true,
+  banner: {
+    js: `/**
+ * 
+ * ${pkg.description}
+ * ${pkg.name} v${pkg.version}
+ * @author ${pkg.author}
+ * @license ${pkg.license}
+ * 
+ */
+    `,
+  },
+  external: ['vscode', 'mocha'],
+});
